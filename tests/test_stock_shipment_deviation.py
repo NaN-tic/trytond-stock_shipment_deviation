9e86acb8e036aef1d797b1e0e99d83001fba538a
resteve <resteve@zikzakmedia.com>
2015-12-01 00:57:46 +0100

    Tests

--- tests/test_stock_shipment_deviation.py
+++ tests/test_stock_shipment_deviation.py
@@ -1,26 +1,14 @@
-# This file is part of stock_shipment_deviation module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import os
-import sys
-import trytond.tests.test_tryton
+# This file is part of the stock_shipment_deviation module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockShipmentDeviationTestCase(unittest.TestCase):
+class StockShipmentDeviationTestCase(ModuleTestCase):
     'Test Stock Shipment Deviation module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_shipment_deviation')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_shipment_deviation')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_shipment_deviation'
 
 
 def suite():
@@ -28,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         StockShipmentDeviationTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
